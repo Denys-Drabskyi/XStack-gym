4a020281e97a54faff547af08630079e0537fd58
Den <marki.drab@gmail.com>
2024-04-14 14:13:25 +0300

    added eureka server

--- main-service/src/main/java/org/example/MainServiceApplication.java
+++ main-service/src/main/java/org/example/MainServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Component;
 
-@SpringBootApplication
 @Component
-public class Main {
+@SpringBootApplication
+public class MainServiceApplication {
   public static void main(String[] args) {
-    SpringApplication.run(Main.class, args);
+    SpringApplication.run(MainServiceApplication.class, args);
   }
 }
